782be78f3a05956c3551b8ae865b07131129c007
Astr0surf3r <astr0surf3r@gmail.com>
2014-02-28 16:16:42 -0800

    primero commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listas::Application.config.secret_token = '2c4f25f4351fe28b24f562c1e65c14272e7d97d8b1b5c07a9da934451769b197f4a9a84d8f73477a5809aaa9402c5e7da90907195f478e29b24a905c1c802e91'
